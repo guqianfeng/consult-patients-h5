e78251e745676982ce90fefcb7416b66c96be0e8
qianfengg <fred_gqf0221@163.com>
2023-02-22 13:55:28 +0800

    css变量

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,26 @@ import { Button as VanButton } from 'vant'
   <div class="app-page">
     <h1>app</h1>
     <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
     <van-button>click</van-button>
   </div>
 </template>
 
 <style scoped lang="scss">
 div.box {
-  width: 375px;
-  height: 100px;
+  --box-size: 100px;
+  height: 30px;
   background-color: pink;
   border: 1px solid #000;
+}
+div.box:nth-of-type(1) {
+  width: var(--box-size);
+}
+div.box:nth-of-type(2) {
+  width: calc(var(--box-size) * 2);
+}
+div.box:nth-of-type(3) {
+  width: calc(var(--box-size) * 3);
 }
 </style>
