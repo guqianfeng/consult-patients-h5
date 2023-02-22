31e72d8bc3aec7a078f28b0312b786f29f5b257f
qianfengg <fred_gqf0221@163.com>
2023-02-22 13:48:33 +0800

    移动端适配

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,16 @@ import { Button as VanButton } from 'vant'
 <template>
   <div class="app-page">
     <h1>app</h1>
+    <div class="box"></div>
     <van-button>click</van-button>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+div.box {
+  width: 375px;
+  height: 100px;
+  background-color: pink;
+  border: 1px solid #000;
+}
+</style>
