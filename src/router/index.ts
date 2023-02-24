1e78a9eb5c572f34e09b67783fac145ce15fcada
qianfengg <fred_gqf0221@163.com>
2023-02-25 00:17:33 +0800

    加载进度

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,19 @@
 import { useUserStore } from './../stores/modules/user'
 import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({
+  showSpinner: false
+})
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
-      component: () => import('@/views/Login/index.vue')
+      component: () => import('@/views/Login/index.vue'),
+      meta: { title: '登录' }
     },
     {
       path: '/',
@@ -15,19 +22,23 @@ const router = createRouter({
       children: [
         {
           path: 'home',
-          component: () => import('@/views/Home/index.vue')
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: '首页' }
         },
         {
           path: 'article',
-          component: () => import('@/views/Article/index.vue')
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: '健康百科' }
         },
         {
           path: 'notify',
-          component: () => import('@/views/Notify/index.vue')
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: '消息通知' }
         },
         {
           path: 'user',
-          component: () => import('@/views/User/index.vue')
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: '个人中心' }
         }
       ]
     },
@@ -39,10 +50,16 @@ const router = createRouter({
 })
 
 router.beforeEach((to) => {
+  NProgress.start()
   const userStore = useUserStore()
   const whiteList = ['/login']
   // 没有登录且去的不是白名单 则去登录页
   if (!userStore.user?.token && !whiteList.includes(to.path)) return '/login'
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || '优医问诊'
+  NProgress.done()
 })
 
 export default router
