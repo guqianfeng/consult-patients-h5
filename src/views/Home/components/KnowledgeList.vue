06bcd670a160e8b13979b4e1ac352cb715298490
qianfengg <fred_gqf0221@163.com>
2023-02-26 15:37:34 +0800

    van-list

--- src/views/Home/components/KnowledgeList.vue
+++ src/views/Home/components/KnowledgeList.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import KnowledgeCard from './KnowledgeCard.vue'
+const list = ref<number[]>([])
+const loading = ref(false)
+const finished = ref(false)
+
+const onLoad = () => {
+  // 异步更新数据
+  // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+  setTimeout(() => {
+    for (let i = 0; i < 5; i++) {
+      list.value.push(list.value.length + 1)
+    }
+
+    // 加载状态结束
+    loading.value = false
+
+    // 数据全部加载完成
+    if (list.value.length >= 15) {
+      finished.value = true
+    }
+  }, 1000)
+}
 </script>
 
 <template>
   <div class="knowledge-list">
-    <knowledge-card v-for="i in 5" :key="i"></knowledge-card>
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <knowledge-card v-for="i in list" :key="i"></knowledge-card>
+    </van-list>
   </div>
 </template>
 
