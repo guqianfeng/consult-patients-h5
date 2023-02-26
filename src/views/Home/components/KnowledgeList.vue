ec1996584bb0f7e2415cb1e7a519ce9629a6f4ec
qianfengg <fred_gqf0221@163.com>
2023-02-26 15:53:04 +0800

    实现加载更多

--- src/views/Home/components/KnowledgeList.vue
+++ src/views/Home/components/KnowledgeList.vue
@@ -1,32 +1,48 @@
 <script setup lang="ts">
-import type { KnowledgeType } from '@/types/consult'
+import type {
+  KnowledgeList,
+  KnowledgeParams,
+  KnowledgeType
+} from '@/types/consult'
 import { ref } from 'vue'
 import KnowledgeCard from './KnowledgeCard.vue'
+import { getKnowledgePage } from '@/services/consult'
 
-defineProps<{
+const props = defineProps<{
   type: KnowledgeType
 }>()
 
-const list = ref<number[]>([])
+const list = ref<KnowledgeList>([])
+const params = ref<KnowledgeParams>({
+  current: 1,
+  pageSize: 10,
+  type: props.type
+})
 const loading = ref(false)
 const finished = ref(false)
 
-const onLoad = () => {
+const onLoad = async () => {
   // 异步更新数据
   // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-  setTimeout(() => {
-    for (let i = 0; i < 5; i++) {
-      list.value.push(list.value.length + 1)
-    }
-
-    // 加载状态结束
-    loading.value = false
-
-    // 数据全部加载完成
-    if (list.value.length >= 15) {
-      finished.value = true
-    }
-  }, 1000)
+  //   setTimeout(() => {
+  //     for (let i = 0; i < 5; i++) {
+  //       list.value.push(list.value.length + 1)
+  //     }
+  //     // 加载状态结束
+  //     loading.value = false
+  //     // 数据全部加载完成
+  //     if (list.value.length >= 15) {
+  //       finished.value = true
+  //     }
+  //   }, 1000)
+  const { data } = await getKnowledgePage(params.value)
+  list.value.push(...data.rows)
+  loading.value = false
+  if (params.value.current >= data.pageTotal) {
+    finished.value = true
+  } else {
+    params.value.current++
+  }
 }
 </script>
 
