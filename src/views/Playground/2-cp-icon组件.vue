64298df8048eb790c994161c356511cdcf719d27
qianfengg <fred_gqf0221@163.com>
2023-02-26 10:38:50 +0800

    cpRadioBtn封装

---
+++ src/views/Playground/2-cp-icon组件.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="playground-page">
+    <h1>playground</h1>
+    <van-button type="primary">click</van-button>
+    <cp-icon name="login-eye-on"></cp-icon>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/Playground/index.vue
+++ src/views/Playground/index.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const options = [
+  {
+    label: '男',
+    value: 1
+  },
+  {
+    label: '女',
+    value: 0
+  }
+]
+const gender = ref(0)
+</script>
 
 <template>
   <div class="playground-page">
     <h1>playground</h1>
-    <van-button type="primary">click</van-button>
-    <cp-icon name="login-eye-on"></cp-icon>
+    <!-- <cp-radio-btn
+      :modelValue="gender"
+      @update:modelValue=";(gender as string | number) = $event"
+      :options="options"
+    ></cp-radio-btn> -->
+    <cp-radio-btn v-model="gender" :options="options"></cp-radio-btn>
   </div>
 </template>
 
