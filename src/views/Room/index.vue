4782310874859c0bcc0b403ad19b332915350b4f
qianfengg <fred_gqf0221@163.com>
2023-03-02 21:18:59 +0800

    问诊室搭结构

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RoomStatus from './components/RoomStatus.vue'
+import RoomAction from './components/RoomAction.vue'
+import RoomMessage from './components/RoomMessage.vue'
+</script>
 
 <template>
   <div class="room-page">
-    <h1>room</h1>
+    <cp-nav-bar title="医生问诊室"></cp-nav-bar>
+    <room-status></room-status>
+    <room-message></room-message>
+    <room-action />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss" scoped>
+.room-page {
+  padding-top: 90px;
+  padding-bottom: 60px;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: var(--cp-bg);
+  .van-pull-refresh {
+    width: 100%;
+    min-height: calc(100vh - 150px);
+  }
+}
+</style>
