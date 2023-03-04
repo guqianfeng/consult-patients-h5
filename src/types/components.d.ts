2a4e9b153b40b0c97610c1e25f80b7c6fb330242
qianfengg <fred_gqf0221@163.com>
2023-03-04 17:21:13 +0800

    rename file

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,6 +1,6 @@
-import CpNavBar from '@/components/cp-nav-bar.vue'
-import CpIcon from '@/components/cp-icon.vue'
-import CpRadioBtn from '@/components/cp-radio-btn.vue'
+import CpNavBar from '@/components/CpNavBar.vue'
+import CpIcon from '@/components/CpIcon.vue'
+import CpRadioBtn from '@/components/CpRadioBtn.vue'
 declare module 'vue' {
   interface GlobalComponents {
     CpNavBar: typeof CpNavBar
