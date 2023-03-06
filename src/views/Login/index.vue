ac455dbabb2ec09de73d3f01e52056cf2392d638
qianfengg <fred_gqf0221@163.com>
2023-03-06 23:40:10 +0800

    跳转qq登录-上

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -121,7 +121,11 @@ onUnmounted(() => {
     <div class="login-other">
       <van-divider>第三方登录</van-divider>
       <div class="icon">
-        <img src="@/assets/qq.svg" alt="" />
+        <a
+          href="https://graph.qq.com/oauth2.0/authorize?client_id=102015968&response_type=token&scope=all&redirect_uri=http%3A%2F%2Fconsult-patients.itheima.net%2Flogin%2Fcallback"
+        >
+          <img src="@/assets/qq.svg" alt="" />
+        </a>
       </div>
     </div>
   </div>
